4e262df2e5ca948c64a6869ce42df784c9686a05
Nicholas Miller <nickmiller708@gmail.com>
2018-11-04 20:26:30 -0500

    adding emotion ranking controller to access highest ranking scores

---
+++ BooksRUsCore/Controllers/EmotionRankingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BooksRUs.Model;
+using BooksRUs.Model.Core.Contracts;
+using BooksRUs.Model.Core;
+using BooksRUs.Model.Core.Contracts.Helpers;
+using BooksRUs.Model.Core.Helpers;
+using System.Collections.Generic;
+
+namespace BooksRUsCore.Controllers
+{
+    public class EmotionRankingController : Controller
+    {
+        public List<EmotionRanking> findBestRanking()
+        {
+            string connString = @"Server=db550.cecsresearch.org;Port=3306;Database=booksrus;Uid=svc_booksrus;Pwd=7FB*@H4pSKA)e&X(ga.;";
+            IBLLFactory emotionRankingBLL = new BLLFactory(connString);
+            var emotionRanking = emotionRankingBLL.emotionRankingBLL;
+            return emotionRanking.findBestRankings();
+        }
+    }
+}
