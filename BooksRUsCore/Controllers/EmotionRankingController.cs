724e92cb59dc27e235a5a54bc4b6fbf90f23aa25
Nicholas Miller <nickmiller708@gmail.com>
2018-11-04 21:03:43 -0500

    only showing one book for emotion

--- BooksRUsCore/Controllers/EmotionRankingController.cs
+++ BooksRUsCore/Controllers/EmotionRankingController.cs
@@ -18,8 +18,19 @@ namespace BooksRUsCore.Controllers
         {
             string connString = @"Server=db550.cecsresearch.org;Port=3306;Database=booksrus;Uid=svc_booksrus;Pwd=7FB*@H4pSKA)e&X(ga.;";
             IBLLFactory emotionRankingBLL = new BLLFactory(connString);
+            List<string> seenEmotions = new List<string>();
+            List<EmotionRanking> bestRankings = new List<EmotionRanking>();
             var emotionRanking = emotionRankingBLL.emotionRankingBLL;
-            return emotionRanking.findBestRankings();
+            var rankings = emotionRanking.findBestRankings();
+            foreach (EmotionRanking rank in rankings)
+            {
+                if (!seenEmotions.Contains(rank.emotion))
+                {
+                    seenEmotions.Add(rank.emotion);
+                    bestRankings.Add(rank);
+                }
+            }
+            return bestRankings;
         }
     }
 }
