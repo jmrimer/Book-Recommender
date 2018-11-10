8b1f766d8285278679fcba333c853b25be23871b
Jason Rimer <jasonmrimer@gmail.com>
2018-11-10 15:26:10 -0500

    Working db context and integration test for Emotion. All others need work.

--- BooksRUsCore/Controllers/EmotionController.cs
+++ BooksRUsCore/Controllers/EmotionController.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using BooksRUs.Model;
-using BooksRUs.Model.Core.Contracts;
+﻿using System.Linq;
 using BooksRUs.Model.Core;
-using BooksRUs.Model.Core.Contracts.Helpers;
-using BooksRUs.Model.Core.Helpers;
 using BooksRUs.Model.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BooksRUsCore.Controllers
 {
     [Route("api/[controller]")]
-
     public class EmotionController : Controller
     {
-        [HttpGet("[action]")]
-        public List<Emotion> GetAllEmotions()
+        private readonly EmotionContext _context;
+
+        public EmotionController(EmotionContext context)
         {
-            string connString = @"Server=db550.cecsresearch.org;Port=3306;Database=booksrus;Uid=svc_booksrus;Pwd=7FB*@H4pSKA)e&X(ga.;";
-            IBLLFactory bllFactory = new BLLFactory(connString);
-            var emotions = bllFactory.emotionBLL;
-            return emotions.selectAllEmotions();
+            _context = context;
+        }
+
+
+        [HttpGet("[action]")]
+        public IQueryable<Emotion> GetAllEmotions()
+        {
+            var emotions = _context.Set<Emotion>();
+            return emotions;
         }
     }
 }
