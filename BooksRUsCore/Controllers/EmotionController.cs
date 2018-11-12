7fbf2d79ca6c84f45f0acf563f26a3592a61401c
Jason Rimer <jasonmrimer@gmail.com>
2018-11-11 19:39:56 -0500

    Working db context and integration test for Emotion. All others need work.

--- BooksRUsCore/Controllers/EmotionController.cs
+++ BooksRUsCore/Controllers/EmotionController.cs
@@ -16,14 +16,25 @@ namespace BooksRUsCore.Controllers
 
     public class EmotionController : Controller
     {
-        [HttpGet("[action]")]
-        public List<Emotion> GetAllEmotions()
+        private readonly EmotionContext _context;
+
+        public EmotionController(EmotionContext context)
         {
-//            string connString = @"Server=db550.cecsresearch.org;Port=3306;Database=booksrus;Uid=svc_booksrus;Pwd=7FB*@H4pSKA)e&X(ga.;";
-            string connString = @"Server=127.0.0.1;Port=3306;Database=BooksRUs;Uid=root;";
-            IBLLFactory bllFactory = new BLLFactory(connString);
-            var emotions = bllFactory.emotionBLL;
-            return emotions.selectAllEmotions();
+            _context = context;
+        }
+            
+        
+        [HttpGet("[action]")]
+        public IQueryable<Emotion> GetAllEmotions()
+        {
+////            string connString = @"Server=db550.cecsresearch.org;Port=3306;Database=booksrus;Uid=svc_booksrus;Pwd=7FB*@H4pSKA)e&X(ga.;";
+//            string connString = @"Server=127.0.0.1;Port=3306;Database=BooksRUs;Uid=root;";
+//            IBLLFactory bllFactory = new BLLFactory(connString);
+//            var emotions = bllFactory.emotionBLL;
+//            return emotions.selectAllEmotions();
+            var emotions = _context.Set<Emotion>(); 
+            return emotions;
+            
         }
     }
 }
--- BooksRUsCore/Startup.cs
+++ BooksRUsCore/Startup.cs
@@ -1,32 +1,46 @@
+using BooksRUs.Model.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace BooksRUsCore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment CurrentEnvironment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment currentEnvironment)
         {
             Configuration = configuration;
+            CurrentEnvironment = currentEnvironment;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (CurrentEnvironment.IsEnvironment("Testing"))
+            {
+                services.AddDbContext<EmotionContext>(options =>
+                    options.UseInMemoryDatabase("TestingDB"));
+            }
+            else
+            {
+                services.AddDbContext<EmotionContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            }
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
