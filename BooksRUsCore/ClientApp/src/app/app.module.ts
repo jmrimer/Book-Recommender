459aebb465ea259b542e8c7c8289b32243b68704
rjtwoh01 <rjtwoh01@louisville.edu>
2018-11-25 08:09:02 -0500

    Unit test (not working currently)

--- BooksRUsCore/ClientApp/src/app/app.module.ts
+++ BooksRUsCore/ClientApp/src/app/app.module.ts
@@ -1,23 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {RecommendationComponent} from "./recommendation/recommendation.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RecommendationComponent } from "./recommendation/recommendation.component";
 import { BookComponent } from "./book/book.component";
 import { BookVoteComponent } from "./book/bookVote.component";
-import {RecommendationService} from "./recommendation/recommendation.service";
-import {VoteComponent} from './vote/vote.component';
-import {VoteContainerComponent} from "./vote/vote-container.component";
-import {BookService} from "./book/book.service";
-import {EmotionService} from "./emotion/emotion.service";
+import { RecommendationService } from "./recommendation/recommendation.service";
+import { VoteComponent } from './vote/vote.component';
+import { VoteContainerComponent } from "./vote/vote-container.component";
+import { BookService } from "./book/book.service";
+import { EmotionService } from "./emotion/emotion.service";
 import { VoteLibraryComponent } from "./votelibrary/votelibrary.component";
-import {VoteService} from "./vote/vote.service";
-import {RatingsComponent} from './ratings/ratings.component';
+import { VoteService } from "./vote/vote.service";
+import { RatingsComponent } from './ratings/ratings.component';
 import { RatingsContainerComponent } from "./ratings/ratings-container/ratings-container.component";
-import {RatingsService} from "./ratings/ratings.service";
+import { RatingsService } from "./ratings/ratings.service";
 
 @NgModule({
   declarations: [
@@ -33,15 +33,15 @@ import {RatingsService} from "./ratings/ratings.service";
     RatingsContainerComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent, pathMatch: 'full'},
+      { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'vote/:id', component: VoteContainerComponent, pathMatch: 'full' },
       { path: 'vote', component: VoteContainerComponent, pathMatch: 'full' },
       { path: 'votelibrary', component: VoteLibraryComponent, pathMatch: 'full' },
-      {path: 'ratings/:id', component: RatingsContainerComponent, pathMatch: 'full'},
+      { path: 'ratings/:id', component: RatingsContainerComponent, pathMatch: 'full' },
     ]),
   ],
   providers: [RecommendationService, BookService, EmotionService, VoteService, RatingsService],
